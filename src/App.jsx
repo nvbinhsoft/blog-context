d350beec2b48895626f017d4faec8b9c3d77c05f
nvbinhsoft <nvbinhsoft@gmail.com>
2024-09-23 14:51:48 +0700

    feat: use user info context

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import UserInfoContext from "./context/UserInfoContext.js";
+import BlogPage from "./components/BlogPage.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+    const userInfo = {
+        username: "Admin",
+        isAdmin: true
+    }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+     <UserInfoContext.Provider value={userInfo}>
+         <BlogPage></BlogPage>
+     </UserInfoContext.Provider>
   )
 }
 
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,14 @@
 import Comment from "./Comment.jsx";
+import UserInfoContext from "../context/UserInfoContext.js";
+import {useContext} from "react";
 
 export default function Post() {
+    const {isAdmin} = useContext(UserInfoContext)
+
     return (
+
         <div>
+            {isAdmin && <button>Delete</button>}
             <h1>Post</h1>
             <p>Welcome to my post!</p>
             <Comment></Comment>
