7eb980f901a4774a68937f42db3b257278a37e7e
nvbinhsoft <nvbinhsoft@gmail.com>
2024-09-23 12:22:07 +0700

    feat: create post component

---
+++ src/components/Post.jsx
@@ -0,0 +1,11 @@
+import Comment from "./Comment.jsx";
+
+export default function Post() {
+    return (
+        <div>
+            <h1>Post</h1>
+            <p>Welcome to my post!</p>
+            <Comment></Comment>
+        </div>
+    )
+}
